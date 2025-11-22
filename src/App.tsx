328bf76ad871269a9ae87d973fc878e5b973cecc
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-11-22 11:16:55 -0300

    creating chat layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 
 import AuthLayout from "./auth/layout/AuthLayout"
+import ChatLayout from "./chat/layout/ChatLayout"
+import ChatPage from "./chat/pages/ChatPage"
 
 function App() {
 
   return (
     <>
-      <AuthLayout />
+      {/* <AuthLayout /> */}
+      <ChatLayout>
+        <ChatPage/>
+      </ChatLayout>
     </>
   )
 }
