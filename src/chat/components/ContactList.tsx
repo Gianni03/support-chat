20dac5fdcab856ecf9e93942677d3887d7107ac3
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-11-27 11:58:04 -0300

    get client list from fake data, and stilig chat persons list

--- src/chat/components/ContactList.tsx
+++ src/chat/components/ContactList.tsx
@@ -1,50 +1,59 @@
 import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
+import { getClients } from '@/fake/fake-data';
+import { useQuery } from '@tanstack/react-query';
 import { NavLink } from 'react-router';
 
 export const ContactList = () => {
+
+  const { data: clients, isLoading } = useQuery({
+    queryKey: ['clients'],
+    queryFn: () => getClients(),
+    staleTime: 1000 * 60 * 5, // 5 minutes
+  })
+
+
   return (
     <ScrollArea className="h-[calc(100vh-64px)]">
       <div className="space-y-4 p-4">
         <div className="space-y-1">
           <h3 className="px-2 text-sm font-semibold">Contacts</h3>
           <div className="space-y-1">
-            <Button variant="secondary" className="w-full justify-start">
-              <div className="h-6 w-6 rounded-full bg-blue-500 mr-2 shrink-0 flex items-center justify-center text-white text-xs">
-                G5
-              </div>
-              G5 Customer
-            </Button>
-            <NavLink
-              to="/chat/1"
-              className="w-full flex items-center justify-start mt-3"
-            >
-              <div className="h-6 w-6 rounded-full bg-green-500 mr-2 shrink-0 flex items-center justify-center text-white text-xs">
-                JD
-              </div>
-              John Doe
-            </NavLink>
-            <NavLink
-              to="/chat/2"
-              className="w-full flex items-center justify-start mt-3"
-            >
-              <div className="h-6 w-6 rounded-full bg-purple-500 mr-2 shrink-0 flex items-center justify-center text-white text-xs">
-                AS
-              </div>
-              Alice Smith
-            </NavLink>
-            <Button variant="ghost" className="w-full justify-start">
-              <div className="h-6 w-6 rounded-full bg-yellow-500 mr-2 shrink-0 flex items-center justify-center text-white text-xs">
-                RJ
-              </div>
-              Robert Johnson
-            </Button>
-            <Button variant="ghost" className="w-full justify-start">
-              <div className="h-6 w-6 rounded-full bg-pink-500 mr-2 shrink-0 flex items-center justify-center text-white text-xs">
-                EW
-              </div>
-              Emma Wilson
-            </Button>
+
+            {isLoading && 
+            <>
+            <div className='animate-pulse h-4 bg-gray-400 rounded'></div>
+            <div className='animate-pulse h-4 bg-gray-300 rounded'></div>
+            <div className='animate-pulse h-4 bg-gray-200 rounded'></div>
+            <div className='animate-pulse h-4 bg-gray-100 rounded'></div>
+            <div className='animate-pulse h-4 bg-gray-400 rounded'></div>
+            <div className='animate-pulse h-4 bg-gray-300 rounded'></div>
+            <div className='animate-pulse h-4 bg-gray-200 rounded'></div>
+            <div className='animate-pulse h-4 bg-gray-100 rounded'></div>
+            </>
+            }
+
+            {
+              clients?.map((client) => (
+                <NavLink
+                  key={client.id}
+                  to={`/chat/${client.id}`}
+                  className={({ isActive }) =>
+                    `flex items-center px-2 py-1 rounded hover:bg-accent hover:text-accent-foreground ${
+                      isActive ? 'bg-accent text-accent-foreground font-medium' : 'text-secondary'
+                    }`
+                  }
+                >
+                  <div className="h-6 w-6 rounded-full bg-gray-300 mr-2 shrink-0 flex items-center justify-center text-white text-xs">
+                    {client.name.charAt(0)}
+                  </div>
+                  <span className="text-gray-600">{client.name}</span>
+                </NavLink>
+              ))} 
+            
+      
+           
+           
           </div>
         </div>
         <div className="pt-4 border-t mt-4">
