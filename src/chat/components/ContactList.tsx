ad1f3993d94fbda198483956b02ddeed927c6cce
Gianni Pasquinelli <giannipasquinelli@gmail.com>
2025-11-29 18:03:43 -0300

    useParams

--- src/chat/components/ContactList.tsx
+++ src/chat/components/ContactList.tsx
@@ -2,16 +2,17 @@ import { Button } from '@/components/ui/button';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { getClients } from '@/fake/fake-data';
 import { useQuery } from '@tanstack/react-query';
-import { NavLink } from 'react-router';
+import { NavLink, useParams } from 'react-router';
 
 export const ContactList = () => {
+  const { clientId } = useParams();
+  console.log('Selected clientId:', clientId);
 
   const { data: clients, isLoading } = useQuery({
     queryKey: ['clients'],
     queryFn: () => getClients(),
     staleTime: 1000 * 60 * 5, // 5 minutes
-  })
-
+  });
 
   return (
     <ScrollArea className="h-[calc(100vh-64px)]">
@@ -19,41 +20,51 @@ export const ContactList = () => {
         <div className="space-y-1">
           <h3 className="px-2 text-sm font-semibold">Contacts</h3>
           <div className="space-y-1">
+            {isLoading && (
+              <>
+                <div className="animate-pulse h-4 bg-gray-400 rounded"></div>
+                <div className="animate-pulse h-4 bg-gray-300 rounded"></div>
+                <div className="animate-pulse h-4 bg-gray-200 rounded"></div>
+                <div className="animate-pulse h-4 bg-gray-100 rounded"></div>
+                <div className="animate-pulse h-4 bg-gray-400 rounded"></div>
+                <div className="animate-pulse h-4 bg-gray-300 rounded"></div>
+                <div className="animate-pulse h-4 bg-gray-200 rounded"></div>
+                <div className="animate-pulse h-4 bg-gray-100 rounded"></div>
+              </>
+            )}
 
-            {isLoading && 
-            <>
-            <div className='animate-pulse h-4 bg-gray-400 rounded'></div>
-            <div className='animate-pulse h-4 bg-gray-300 rounded'></div>
-            <div className='animate-pulse h-4 bg-gray-200 rounded'></div>
-            <div className='animate-pulse h-4 bg-gray-100 rounded'></div>
-            <div className='animate-pulse h-4 bg-gray-400 rounded'></div>
-            <div className='animate-pulse h-4 bg-gray-300 rounded'></div>
-            <div className='animate-pulse h-4 bg-gray-200 rounded'></div>
-            <div className='animate-pulse h-4 bg-gray-100 rounded'></div>
-            </>
-            }
-
-            {
-              clients?.map((client) => (
-                <NavLink
-                  key={client.id}
-                  to={`/chat/${client.id}`}
-                  className={({ isActive }) =>
-                    `flex items-center px-2 py-1 rounded hover:bg-accent hover:text-accent-foreground ${
-                      isActive ? 'bg-accent text-accent-foreground font-medium' : 'text-secondary'
-                    }`
-                  }
+            {clients?.map((client) => (
+              <NavLink
+                key={client.id}
+                to={`/chat/${client.id}`}
+                className={({ isActive }) =>
+                  `flex items-center px-2 py-1 rounded hover:bg-accent hover:text-accent-foreground ${
+                    isActive
+                      ? 'bg-accent text-accent-foreground font-medium'
+                      : 'text-secondary'
+                  }`
+                }
+              >
+                <div
+                  className={`h-6 w-6 rounded-full mr-2 shrink-0 flex items-center justify-center text-xs
+                    ${
+                      clientId === client.id
+                        ? 'bg-blue-300 text-blue-700 font-medium'
+                        : 'bg-gray-300'
+                    }`}
                 >
-                  <div className="h-6 w-6 rounded-full bg-gray-300 mr-2 shrink-0 flex items-center justify-center text-white text-xs">
-                    {client.name.charAt(0)}
-                  </div>
-                  <span className="text-gray-600">{client.name}</span>
-                </NavLink>
-              ))} 
-            
-      
-           
-           
+                  {client.name.charAt(0)}
+                  {client.name.charAt(1)}
+                </div>
+                <span
+                  className={`transition-all duration-300 ${
+                    clientId === client.id ? 'text-blue-700 font-medium' : 'text-gray-600'
+                  }`}
+                >
+                  {client.name}
+                </span>
+              </NavLink>
+            ))}
           </div>
         </div>
         <div className="pt-4 border-t mt-4">
